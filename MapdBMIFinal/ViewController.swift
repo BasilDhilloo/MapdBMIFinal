2eb76fc7c038249d117793c3b0f9890d3cf7b91b
BasilDhilloo <bdhilloo@gmail.com>
2021-12-18 17:29:32 -0500

    First Commit

--- MapdBMIFinal/ViewController.swift
+++ MapdBMIFinal/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Name: UITextField!
+    
+    
+    
+    @IBOutlet weak var Weight: UITextField!
+    
+    
+    @IBOutlet weak var Height: UITextField!
+    
+    
+    @IBOutlet weak var BMI: UILabel!
+    
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func Result(_ sender: Any) {
+        if(self.Height.text!=="" && self.Weight.text!==""){
+            return
+        }
+        
+        let height = Float(self.Height.text!)!
+        let weight = Float(self.Weight.text!)!
+        
+        let heightsquare = height*height
+        
+         
+        var bmi: Float =
+        weight/heightsquare
+        self.BMI.text = "BMI \(self.Name.text!), Your BMI is \(String(bmi))"
+           
+    }
 
 }
 
